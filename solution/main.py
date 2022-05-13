0240e9fdc05658bb580c1d449185fbe5eb13bf83
anuvgupta <me@anuv.me>
2022-05-13 04:43:43 -0500

    solution code from phue bridge gateway service tests

---
+++ solution/main.py
@@ -0,0 +1,21 @@
+# phue iot challenge solution
+
+from tests import test_a, test_b
+
+target = "192.168.0.100"
+# target = "phue.anuv.me"
+# target = "localhost:8080"
+
+
+
+# main
+def main():
+    print("phue gateway service tests")
+    print("")
+
+    # test_a(target)
+    test_b(target)
+
+# entry point
+if __name__ == "__main__":
+    main()
---
+++ solution/tests.py
@@ -0,0 +1,29 @@
+# phue test routine
+
+from utils import RGBtoXY, EnhanceColor
+
+from phue import Bridge
+import json
+import math
+
+
+def test_a(target):
+    print("testing connection with target: {}".format(target))
+    b = Bridge(target)
+    print("")
+    print(b.connect())
+    print("")
+    print(b.get_api())
+
+def test_b(target):
+    print("testing with target: {}".format(target))
+    bridge = Bridge(target)
+    bridge.connect()
+    my_lights = bridge.get_light_objects('name')
+
+    my_lights['Small lamp'].on = True
+    x,y = RGBtoXY(0, 0, 0)
+    bridge.set_light('Small lamp', 'xy', [x,y])
+    # to get the scene id
+    #print(json.dumps(bridge.get_scene(), indent=2))
+    bridge.activate_scene("1", "Fo0TM840Hsy0wcG")
